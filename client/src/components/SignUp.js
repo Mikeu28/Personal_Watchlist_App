29e67d5831036229cc53cae5ec0b485cfff55f7b
MichaelUrezzio <mikeurezzio@gmail.com>
2023-09-25 22:42:05 -0400

    put a proxy in package.json

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -18,7 +18,7 @@ function SignUp () {
 
     function handleSubmit ( e ) {
         e.preventDefault()
-        fetch( 'http://127.0.0.1:5555/users', {
+        fetch( '/users', {
             method: "POST",
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify( form )
